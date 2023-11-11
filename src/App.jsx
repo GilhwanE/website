ee051401d006f5f7b391201e6577a064dc2a6f70
gilhwan <dyghks123@gmail.com>
2023-11-11 15:33:40 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './app.scss';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <section>Home</section>
+      <section>Parallax</section>
+      <section>Content</section>
+      <section>Parallax</section>
+      <section>Work1</section>
+      <section>Work2</section>
+      <section>Work3</section>
+    </>
+  );
+};
+export default App;
