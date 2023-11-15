5a6023bcbaa3a7d9889e4e48cf8df0f022886138
gilhwan <dyghks123@gmail.com>
2023-11-15 23:28:33 +0900

    💄 Fix App scss, component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import './app.scss';
 
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import Parallax from './components/parallax/Parallax';
 
 const App = () => {
   return (
     <>
       <section id="Home">
         <Navbar />
+        <Home />
       </section>
-      <section id="Content">Parallax</section>
+      <section id="Content">
+        {/* type="" type={} prop을 전달하는 두가지 방식 중 prop의 값을 고정적으로 사용하기 위해서 첫번째 코드를 사용함 */}
+        <Parallax type="content" />
+      </section>
       <section>Content</section>
-      <section id="Work">Parallax</section>
+      <section id="Work">
+        <Parallax type="work" />
+      </section>
       <section>Work1</section>
       <section>Work2</section>
       <section>Work3</section>
