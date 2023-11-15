1dcb693acf7be18ffc185acdedd0221aa1d0e6ca
gilhwan <dyghks123@gmail.com>
2023-11-15 23:27:27 +0900

    💄 Add to Home scss style

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,23 @@
+import './home.scss';
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="wrap">
+        <div className="textBox">
+          <h3>Welcome JipLink</h3>
+          <h2>We gathered a recruitment site platform.</h2>
+          <div>
+            <button></button>
+          </div>
+          <img src="" alt="" />
+        </div>
+        <div className="imageBox">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
